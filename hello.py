4f90d4872b10bd3396eb24baf15f66ca6cade641
themaggotlestat <corey.gilmore1994@gmail.com>
2026-09-14 19:32:13 -0500

    Initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+name = input("What is your name?")
+
+location = input("Where do you want to live?")
+
+career =input("What career are you pursuing?")
+
+age = int(input("How old are you?"))
+
+print(age * 5)
+
+print(f"Hello, I'm {name}! I am {age} years old. I plan to live in {location} and work as a {career}.")
