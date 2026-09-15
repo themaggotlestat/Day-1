662bfd84393c5de24d780ec77ec8790965a9a674
themaggotlestat <corey.gilmore1994@gmail.com>
2026-09-14 19:52:51 -0500

    Update hello.py
    Add age calculation

--- hello.py
+++ hello.py
@@ -6,6 +6,6 @@ career =input("What career are you pursuing?")
 
 age = int(input("How old are you?"))
 
-print(age * 5)
-
 print(f"Hello, I'm {name}! I am {age} years old. I plan to live in {location} and work as a {career}.")
+
+print(f"In 5 years, I will be {age + 5} years old")
